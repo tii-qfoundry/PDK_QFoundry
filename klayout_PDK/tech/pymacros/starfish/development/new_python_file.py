0a40431c0b3d8a8c8c69db74dcdc21ef8bb24327
Juan E. Villegas <jevillegasd@gmail.com>
2023-10-09 17:41:51 +0400

    update layers

--- klayout_PDK/tech/pymacros/starfish/development/new_python_file.py
+++ klayout_PDK/tech/pymacros/starfish/development/new_python_file.py
@@ -6,8 +6,8 @@ from kqcircuits.klayout_view import KLayoutView
 from kqcircuits.elements.waveguide_composite import WaveguideComposite
 from kqcircuits.elements.waveguide_composite import Node
 
-view = KLayoutView(current=True, initialize=False)
-layout = KLayoutView.get_active_layout()
+#view = KLayoutView(current=True, initialize=False)
+#layout = KLayoutView.get_active_layout()
 
 
 
@@ -15,9 +15,9 @@ layout = KLayoutView.get_active_layout()
 
 #top_cell = layout.create_cell('top')
 
-layout_view = view.layout_view
-cell_view = view.get_active_cell_view()
-cell_view.cell_name = 'top'  # Shows the new cell
+#layout_view = view.layout_view
+#cell_view = view.get_active_cell_view()
+#cell_view.cell_name = 'top'  # Shows the new cell
         
 #wg_cell = WaveguideComposite.create(layout, nodes=[Node((-1.0, 750.0)), Node((-901.0, -750.0)), Node((-901.0, -1000.0)), Node((-1.0, -1000.0)), Node((-1.0, -1900.0), length_before=2000.0)])
 #top_cell.insert(pya.DCellInstArray(wg_cell.cell_index(), pya.DTrans()))
