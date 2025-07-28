5ad4352a5a5a176c30453aae4ee29680ccc0c435
Juan E. Villegas <jevillegasd@gmail.com>
2025-07-28 11:58:21 +0400

    Update Manhattan.py

--- qfoundry/tech/pymacros/qfoundry/junctions/Manhattan.py
+++ qfoundry/tech/pymacros/qfoundry/junctions/Manhattan.py
@@ -7,8 +7,13 @@ from math import pi
 # Juan E. Villegas, Nov. 2023
 
 from kqcircuits.util.symmetric_polygons import polygon_with_vsym
-from qfoundry.junctions.utils import arc, draw_junction, draw_pad, draw_patch, draw_pad, draw_patch_openning
+from qfoundry.junctions.utils import arc, draw_junction, draw_pad, draw_patch, draw_patch_openning
 from qfoundry.utils import _add_shapes
+
+NEGATIVE_LAYERS = [
+    pya.LayerInfo(1, 0),
+    pya.LayerInfo(130, 1),
+]
 
 
     
@@ -69,12 +74,21 @@ class Manhattan(pya.PCellDeclarationHelper):
             dbu = self.layout.dbu
             
             #Junction
-            finger_shapes = draw_junction(self.angle, self.inner_angle, self.junction_width_b, self.junction_width_t, self.finger_size, self.mirror_offset, self.offset_compensation, self.finger_overshoot, self.finger_overlap, pya.DPoint(0, 0), dbu=dbu)
+            finger_shapes = draw_junction(angle = self.angle, 
+                                          inner_angle = self.inner_angle, 
+                                          junction_width_b = self.junction_width_b, 
+                                          junction_width_t = self.junction_width_t, 
+                                          finger_size = self.finger_size, 
+                                          mirror_offset = self.mirror_offset, 
+                                          offset_compensation = self.offset_compensation, 
+                                          finger_overshoot = self.finger_overshoot, 
+                                          finger_overlap = self.finger_overlap, 
+                                          bottom_lead_comp = 0, center = pya.DPoint(0, 0), dbu = dbu)
             conn_shapes = self.draw_connectors(pya.DPoint(0, 0))
-            layer = self.layout.layer(self.l_layer)
-            _add_shapes(self.cell, finger_shapes, layer)
-            _add_shapes(self.cell, conn_shapes, layer)
-            
+            layer_jj = self.layout.layer(self.l_layer)
+            _add_shapes(self.cell, finger_shapes, layer_jj)
+            _add_shapes(self.cell, conn_shapes, layer_jj)
+
             # Capacitor
             if self.draw_cap:
                 cap_shape = draw_pad(self.cap_w, self.cap_h, self.cap_gap, dbu = dbu)
@@ -82,60 +96,90 @@ class Manhattan(pya.PCellDeclarationHelper):
                 patch_open_shape = []
                 patch_shape = []
                 
-                # Patches
-                if self.draw_patch:
-                    # Patch opening in base metal layer
-                    center = pya.DPoint(0, 0)
-                    top_height = self.conn_height+self.cap_gap/2-self.finger_size*sin(_angle)
-                    patch_top = draw_patch_openning(
-                        self.finger_size, 
-                        self.conn_width, 
-                        top_height, 
-                        self.angle, 
-                        self.inner_angle, 
-                        gap = self.patch_gap
-                    )
-                    
-                    bot_height = self.conn_height+self.cap_gap/2-self.finger_size*cos(_angle)
-                    patch_bot = draw_patch_openning(
-                        self.finger_size, 
-                        self.conn_width, 
-                        bot_height, 
-                        self.angle-90, 
-                        self.inner_angle,   
-                        gap = self.patch_gap
-                    )
+                metal_neg = pya.Box(-(self.cap_w+80)/dbu/2, -(self.cap_h+40+self.cap_gap/2)/dbu,
+                                    (self.cap_w+80)/dbu/2, (self.cap_h+40+self.cap_gap/2)/dbu)
 
-                    patch_open_shape = [
-                      (pya.DTrans(0, False, center.x,center.y) * patch_top).to_itype(dbu),
-                      (pya.DTrans(0, False, center.x,center.y-bot_height) * patch_bot).to_itype(dbu)
-                    ]
+                region_pos = pya.Region(cap_shape).merged()
+                region_neg = pya.Region(metal_neg).merged()- pya.Region(cap_shape).merged()
+            else:
+                # If no capacitor is drawn, we still need to define the regions
+                region_pos = pya.Region()
+                region_neg = pya.Region()
+                
+            # Patches
+            if self.draw_patch:
+                # Use the updated draw_patch function
+                patch_shape = draw_patch(
+                    self.finger_size, 
+                    self.cap_gap, 
+                    self.conn_width, 
+                    self.conn_height, 
+                    self.angle, 
+                    self.inner_angle, 
+                    self.patch_scratch, 
+                    self.patch_clearance,
+                    center=pya.DPoint(0, 0), 
+                    dbu=dbu
+                )
+                
+                # Patch opening in base metal layer
+                center = pya.DPoint(0, 0)
+                top_height = self.conn_height+self.cap_gap/2-self.finger_size*sin(_angle)
+                patch_top = draw_patch_openning(
+                    self.finger_size, 
+                    self.conn_width, 
+                    top_height, 
+                    self.angle, 
+                    self.inner_angle, 
+                    gap = self.patch_gap
+                )
+                
+                bot_height = self.conn_height+self.cap_gap/2-self.finger_size*cos(_angle)
+                patch_bot = draw_patch_openning(
+                    self.finger_size, 
+                    self.conn_width, 
+                    bot_height, 
+                    self.angle-90, 
+                    self.inner_angle,   
+                    gap = self.patch_gap
+                )
 
-   
-                
-                layerm = self.layout.layer(pya.LayerInfo(1, 0))
-                
+                patch_open_shape = [
+                    (pya.DTrans(0, False, center.x,center.y) * patch_top).to_itype(dbu),
+                    (pya.DTrans(0, False, center.x,center.y-bot_height) * patch_bot).to_itype(dbu)
+                ]
+                region_pos = region_pos - pya.Region(patch_open_shape).merged()
+                region_neg = region_neg + pya.Region(patch_open_shape).merged()
+            
+            # Label handling
+            if self.draw_cap:
                 trans = pya.Trans(pya.Trans.R0, (-self.cap_w/2+10)/dbu, (self.cap_h-10)/dbu)     
                 cell_label = self.layout.create_cell("TEXT", "Basic", {"text":self.label, "mag":20,"layer": pya.LayerInfo(1, 0) })
                 cell_instance_lbl = pya.CellInstArray(cell_label.cell_index(),trans)
-                self.cell.insert(cell_instance_lbl)  
+            else:
+                cell_instance_lbl = pya.CellInstArray(pya.CellIndex(0), trans)
+            
+            # Add shapes to the layout
+            layer_cap = self.layout.layer(self.cap_layer)
 
-                
-                metal_neg = pya.Box(-(self.cap_w+80)/dbu/2, -(self.cap_h+40+self.cap_gap/2)/dbu,
-                                    (self.cap_w+80)/dbu/2, (self.cap_h+40+self.cap_gap/2)/dbu)
-                
+            # Apply layer logic based on lithography type
+            if layer_cap in [self.layout.layer(layer) for layer in NEGATIVE_LAYERS]: 
+                # Negative lithography: use separate layers for positive and negative regions
                 layer_add = self.layout.layer(pya.LayerInfo(131, 1))
-                
-                region_pos = pya.Region(cap_shape).merged() - pya.Region(patch_open_shape).merged()
                 self.cell.shapes(layer_add).insert(region_pos) 
-                    
-                region_neg = pya.Region(metal_neg).merged()- pya.Region(cap_shape).merged() + pya.Region(patch_open_shape).merged()
-                self.cell.shapes(layerm).insert(region_neg)  
-                
-                layer = self.layout.layer(self.patch_layer)
-                _add_shapes(self.cell, patch_shape, layer)  
-    
-    
+                self.cell.shapes(layer_cap).insert(region_neg)
+                self.cell.insert(cell_instance_lbl)    
+            else:
+                # Positive lithography: insert label and subtract from positive region
+                self.cell.insert(cell_instance_lbl)
+                # Flatten and subtract the label region from region_pos if needed
+                self.cell.shapes(layer_cap).insert(region_pos)
+
+            # Add patch shapes to patch layer
+            layer_patch = self.layout.layer(self.patch_layer)
+            _add_shapes(self.cell, patch_shape, layer_patch)
+
+
     def draw_connectors(self, center=pya.DPoint(0, 0)):
         dbu = self.layout.dbu
         size= self.finger_size
